d572eb0c47f430b10327088ae42aee8d4d7ebd42
anerilohakane <aneridelxn@gmail.com>
2026-09-23 10:59:25 +0530

    build issue

--- src/lib/mongodb.ts
+++ src/lib/mongodb.ts
@@ -2,10 +2,6 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI!;
 
-if (!MONGODB_URI) {
-  throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
-
 /**
  * Global is used here to maintain a cached connection across hot reloads
  * in development. This prevents connections growing exponentially
@@ -18,6 +14,11 @@ if (!cached) {
 }
 
 async function connectToDatabase() {
+  if (!MONGODB_URI) {
+    console.warn("MONGODB_URI is not defined. Skipping database connection.");
+    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
+  }
+
   if (cached.conn) {
     return cached.conn;
   }
